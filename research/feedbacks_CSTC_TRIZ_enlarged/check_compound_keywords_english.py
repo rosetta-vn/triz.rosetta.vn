c093d5b0c285a26134d35f8f7c31fa846c4db65e
rosettavn <doanminhdang@gmail.com>
2019-02-19 17:06:32 +0100

    Add english meaning in keywords of TSK feedbacks

--- research/feedbacks_CSTC_TRIZ_enlarged/check_compound_keywords_english.py
+++ research/feedbacks_CSTC_TRIZ_enlarged/check_compound_keywords_english.py
@@ -8,7 +8,7 @@ Created on Tue Feb 19 2019
 @author: dang
 
 Example: (remember to put file names with space in double quote)
-python check_compound_keywords.py folder_input keywords.csv output_file.csv
+python check_compound_keywords_english.py folder_input keywords.csv output_file.csv
 """
 
 import sys
@@ -21,16 +21,19 @@ import Csv_Excel
 
 def read_keywords(file_keywords, keyword_delimiter=':'):
     """
-    Read a text file in CSV format, take the first column to make the list of keywords
-    Keyword file contains rows in the syntax <keyword>:<long description>, example:
-    lý thú: Người học đánh giá nội dung khóa học là lý thú
-    Return a list of keywords
+    Read a text file in CSV format, take the first TWO columns to make the list of keywords
+    Keyword file contains rows in the syntax <first type keyword>:<second type keyword>:<long description>, example:
+    lý thú: interesting : Người học đánh giá nội dung khóa học là lý thú
+    Return two list of keywords for two types
     """
     table = Csv_Table.read_csv_table(file_keywords, delimiter = keyword_delimiter)
-    list_keywords = Csv_Table.parse_table_column(table, 1)[0] # take out the first column
-    list_keywords = [word.strip() for word in list_keywords]
-    list_keywords_filtered = list(filter(None, list_keywords)) # remove empty items
-    return list_keywords_filtered
+    selected_columns = Csv_Table.parse_table_column(table, 2)
+    list_keywords = [[word.strip() for word in each_list_keywords] for each_list_keywords in selected_columns]
+    for kk in range(len(list_keywords[0]) - 1, -1, -1):
+        if not list_keywords[0][kk]: # remove empty items if the first column is empty
+            del list_keywords[0][kk]
+            del list_keywords[1][kk]
+    return list_keywords[0], list_keywords[1]
 
 
 def check_keywords_file(list_keywords, file_text):
@@ -60,13 +63,14 @@ def check_compound_keywords_file(list_keywords, file_text):
     return matching
 
 
-def mass_check_folder(folder, list_keywords):
+def mass_check_folder(folder, list_keywords, list_keywords_info):
     """
     Check keywords for every file inside the folder, recursively
     Output table of matching arrays, with the first row is list of keywords
     """
+    info_row = ['Meaning'] + list(list_keywords_info)
     header_row = ['Keywords'] + list(list_keywords)
-    table = [header_row]
+    table = [info_row, header_row]
     file_paths = []
     for root, dirnames, filenames in os.walk(folder):
         for filename in fnmatch.filter(filenames, '*.txt'):
@@ -79,15 +83,15 @@ def mass_check_folder(folder, list_keywords):
     sum_row = ['Sum per keywords']
     for column in range(1, len(header_row)):
         column_sum = 0
-        for row in range(1, len(file_paths)+1):
+        for row in range(2, len(file_paths)+2):
             column_sum += table[row][column]
         sum_row.append(column_sum)
     table.append(sum_row)
     return table
 
 def mass_check_keywords(folder, file_keywords):
-    list_keywords = read_keywords(file_keywords)
-    table = mass_check_folder(folder, list_keywords)
+    list_keywords_main, list_keywords_aux = read_keywords(file_keywords)
+    table = mass_check_folder(folder, list_keywords_main, list_keywords_aux)
     return table
 
 def check_keywords(folder, file_keywords, file_output):
@@ -97,7 +101,8 @@ def check_keywords(folder, file_keywords, file_output):
     # The command below is just to test converting XLS to CSV again, no need to use
     #Csv_Excel.xls_to_csv(file_output.replace('csv', 'xls'), file_output.replace('.csv','_2.csv'), delimiter=',')
 
-# Main operation, when calling: python check_compound_keywords.py data_individual keywords.csv matrix_keywords.csv
+# Main operation, when calling: python check_compound_keywords_english.py data_individual keywords_uutien_1_english.csv matrix_keywords_group1_english.csv
+
 if __name__ == "__main__":
     folder_input = str(sys.argv[1])
     keywords = str(sys.argv[2])
